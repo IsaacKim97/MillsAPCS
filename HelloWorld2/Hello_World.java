4feaf5fbdbeb28bdc399474c16e09c38ddb2da97
IsaacKim97 <isaackim97@gmail.com>
2015-08-18 09:23:15 -0700

    Who said that?
    The computer has gained sentience. It needs to die

---
+++ HelloWorld2/Hello_World.java
@@ -0,0 +1,14 @@
+
+/**
+ * This will do a simple println. Our first java code.
+ * 
+ * @author Isaac Kim
+ * @version August 18,2015
+ */
+public class Hello_World
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Isaac.");
+    }
+}
